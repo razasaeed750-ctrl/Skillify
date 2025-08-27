b5acd62d9f66ca7651daa2e9f19706d35ef04ebd
razasaeed750-ctrl <razasaeed750@gmail.com>
2025-08-27 20:19:44 +0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,43 +1,24 @@
-const CACHE_NAME = "skillify-cache-v1";
-const urlsToCache = [
-  "/",               // root
-  "/index.html",
-  "/manifest.json",
-  "/icons/icon-192x192.png",
-  "/icons/icon-512x512.png"
-];
-
-// Install SW & cache files
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
+    caches.open("raza-site-cache").then(cache => {
+      return cache.addAll([
+        "index.html",
+        "manifest.json",
+        "icons/icon-192.png",
+        "icons/icon-512.png",
+        "downloads/cv-template.docx",
+        "downloads/cv-template.pdf",
+        "downloads/ppt-template.pptx",
+        "downloads/ppt-template2.pptx"
+      ]);
     })
   );
-  console.log("✅ Service Worker Installed");
 });
 
-// Fetch cached files
 self.addEventListener("fetch", event => {
   event.respondWith(
     caches.match(event.request).then(response => {
       return response || fetch(event.request);
     })
   );
-});
-
-// Activate SW & clear old caches
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(cacheNames =>
-      Promise.all(
-        cacheNames.map(cache => {
-          if (cache !== CACHE_NAME) {
-            return caches.delete(cache);
-          }
-        })
-      )
-    )
-  );
-  console.log("✅ Service Worker Activated");
 });
